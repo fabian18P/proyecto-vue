489e22664acebd970a1fe54f00d9416fc039479d
fabian <fabian.alexander.portuguez@gmail.com>
2025-02-24 22:01:33 -0500

    Imagenes

--- vue-project/src/data/home.js
+++ vue-project/src/data/home.js
@@ -2,9 +2,9 @@ export const home = {
     nombre: "PLANETA PLANTA",
     autor: "Julio Cesar Matos Lizana",
     logo: [
-      {descripcion: "Blanco", url: "/src/assets/logoBlanco.svg"},
-      {descripcion: "Negro", url: "/src/assets/logoNegro.svg"},
-      {descripcion: "NegroTrasparente", url: "/src/assets/logoNegroTrasparente.svg"},
-      {descripcion: "Color", url: "/src/assets/logoColor.svg"}
+      {descripcion: "Blanco", url: "@/src/assets/logoBlanco.svg"},
+      {descripcion: "Negro", url: "@/src/assets/logoNegro.svg"},
+      {descripcion: "NegroTrasparente", url: "@/src/assets/logoNegroTrasparente.svg"},
+      {descripcion: "Color", url: "@/src/assets/logoColor.svg"}
     ],
   };
